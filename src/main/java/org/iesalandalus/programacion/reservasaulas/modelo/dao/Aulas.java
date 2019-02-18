05aced6790c327db7ae363bc987f6366e727f484
JFQueroDev <JFQueroDev@Base-LPTP>
2019-02-18 19:50:40 +0100

    Modificada la clase Aulas para usar ArrayList y pasados los test

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Aulas.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Aulas.java
@@ -1,19 +1,17 @@
 package org.iesalandalus.programacion.reservasaulas.modelo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.naming.OperationNotSupportedException;
-
 import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Aula;
 
 public class Aulas {
 
-	private static final int MAX_AULAS = 120;
-	private int numAulas;
-	private Aula[] coleccionAulas;
+	private List<Aula> coleccionAulas;
 
 	/* Constructores */
 	public Aulas() {
-		coleccionAulas = new Aula[MAX_AULAS];
-		numAulas = 0;
+		coleccionAulas = new ArrayList<>();
 	}
 
 	public Aulas(Aulas aulas) {
@@ -26,68 +24,39 @@ public class Aulas {
 			throw new IllegalArgumentException("No se pueden copiar aulas nulas.");
 		}
 		coleccionAulas = copiaProfundaAulas(aulas.coleccionAulas);
-		numAulas = aulas.numAulas;
 	}
 
-	private Aula[] copiaProfundaAulas(Aula[] aulas) {
-		Aula[] copiaAulas = new Aula[aulas.length];
-		for (int i = 0; i < aulas.length && aulas[i] != null; i++) {
-			copiaAulas[i] = new Aula(aulas[i]);
+	private List<Aula> copiaProfundaAulas(List<Aula> aulas) {
+		List<Aula> copiaAulas = new ArrayList<>();
+		for (Aula aula : aulas) {
+			copiaAulas.add(new Aula(aula));
 		}
 		return copiaAulas;
 	}
 
-	public Aula[] getAulas() {
+	public List<Aula> getAulas() {
 		return copiaProfundaAulas(coleccionAulas);
 	}
 
 	public int getNumAulas() {
-		return numAulas;
+		return coleccionAulas.size();
 	}
 
 	public void insertar(Aula aula) throws OperationNotSupportedException {
 		if (aula == null) {
 			throw new IllegalArgumentException("No se puede insertar un aula nula.");
 		}
-		int indice = buscarIndiceAula(aula);
-		if (!indiceNoSuperaTamano(indice)) {
-			coleccionAulas[indice] = new Aula(aula);
-			numAulas++;
+		if (coleccionAulas.contains(aula)) {
+			throw new OperationNotSupportedException("El aula ya existe.");
 		} else {
-			if (indiceNoSuperaCapacidad(indice)) {
-				throw new OperationNotSupportedException("El aula ya existe.");
-			} else {
-				throw new OperationNotSupportedException("No se aceptan mas aulas.");
-			}
+			coleccionAulas.add(new Aula(aula));
 		}
-	}
-
-	private int buscarIndiceAula(Aula aula) {
-		int indice = 0;
-		boolean aulaEncontrada = false;
-		while (indiceNoSuperaTamano(indice) && !aulaEncontrada) {
-			if (coleccionAulas[indice].equals(aula)) {
-				aulaEncontrada = true;
-			} else {
-				indice++;
-			}
-		}
-		return indice;
-	}
-
-	private boolean indiceNoSuperaTamano(int indice) {
-		return indice < numAulas;
-	}
-
-	private boolean indiceNoSuperaCapacidad(int indice) {
-		return indice < MAX_AULAS;
 	}
 
 	public Aula buscar(Aula aula) {
-		int indice = 0;
-		indice = buscarIndiceAula(aula);
-		if (indiceNoSuperaTamano(indice)) {
-			return new Aula(coleccionAulas[indice]);
+		int indice = coleccionAulas.indexOf(aula);
+		if (indice!= -1) {
+			return new Aula(coleccionAulas.get(indice));
 		} else {
 			return null;
 		}
@@ -97,27 +66,15 @@ public class Aulas {
 		if (aula == null) {
 			throw new IllegalArgumentException("No se puede borrar un aula nula.");
 		}
-		int indice = buscarIndiceAula(aula);
-		if (indiceNoSuperaTamano(indice)) {
-			desplazarUnaPosicionHaciaIzquierda(indice);
-		} else {
+		if (!coleccionAulas.remove(aula)) {
 			throw new OperationNotSupportedException("El aula a borrar no existe.");
 		}
-
 	}
 
-	private void desplazarUnaPosicionHaciaIzquierda(int indice) {
-		for (int i = indice; i < numAulas - 1; i++) {
-			coleccionAulas[i] = new Aula(coleccionAulas[i + 1]);
-		}
-		coleccionAulas[numAulas] = null;
-		numAulas--;
-	}
-
-	public String[] representar() {
-		String[] representacion = new String[numAulas];
-		for (int i = 0; indiceNoSuperaTamano(i); i++) {
-			representacion[i] = new Aula(coleccionAulas[i]).toString();
+	public List<String> representar() {
+		List<String> representacion = new ArrayList<>();
+		for (Aula aula : coleccionAulas) {
+			representacion.add(new Aula(aula).toString());
 		}
 		return representacion;
 	}
